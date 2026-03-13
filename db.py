d6d19b6b1672b1f5e394829bc6b44f8165d49a49
LAB_B_REAL <labb@continental.edu.pe>
2026-03-13 15:43:58 -0500

    Fix: Usando host y credenciales de AlwaysData

--- db.py
+++ db.py
@@ -2,9 +2,19 @@ import mysql.connector
 
 def get_connection():
     conexion = mysql.connector.connect(
-        host="localhost",       # O la IP de tu servidor
-        user="root",            # Tu usuario de MySQL (suele ser root en local)
-        password="",            # Tu contraseña (vacía si usas XAMPP por defecto)
-        database="sebastianmateo_616" # El nombre de la base de datos que creaste
+        # El host siempre es este para tu cuenta:
+        host="mysql-sebastianmateo.alwaysdata.net", 
+        
+        # OJO: AlwaysData suele usar tu nombre de cuenta como prefijo.
+        # Basado en tu URL de administración, tu usuario real es:
+        user="sebastianmateo", 
+        
+        # La contraseña que elegiste al crear el usuario en AlwaysData
+        password="Baldeon_2000", 
+        
+        # El nombre de la base de datos (tal cual sale en tu captura)
+        database="sebastianmateo_616",
+        
+        port=3306 
     )
     return conexion
