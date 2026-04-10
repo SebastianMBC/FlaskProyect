0d03a7ca4929d322a12a67bce68154f1a16ec641
SebatianMBC <sebastian.mateo.bc@gmail.com>
2026-04-10 01:39:26 -0500

    Newww Commit

--- db.py
+++ db.py
@@ -1,11 +1,13 @@
 import mysql.connector
+import os
 
 def get_connection():
     conexion = mysql.connector.connect(
-        host="mysql-sebastianmateo.alwaysdata.net", 
-        user="sebastianmateo", 
-        password="Baldeon_2000", 
-        database="sebastianmateo_616",
-        port=3306 
+        host=os.environ.get("DB_HOST", "mysql-sebastianmateo.alwaysdata.net"),
+        database=os.environ.get("DB_NAME", "sebastianmateo_flasktest"),
+        user=os.environ.get("DB_USER", "sebastianmateo_smbc_2000"),
+        password=os.environ.get("DB_PASSWORD", "986993399"),
+        port=3306,
+        connection_timeout=10
     )
     return conexion
